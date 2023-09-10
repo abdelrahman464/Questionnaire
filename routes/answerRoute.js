6bff39440e87adcea07ca909b056a755af0e0a2e
Abdelrhman Alaa <bedoalaa2155@gmail.com>
2023-09-10 22:39:20 +0300

    answerRoute

--- routes/answerRoute.js
+++ routes/answerRoute.js
@@ -8,7 +8,7 @@ const {
 
 const router = express.Router();
 
-router.route("/:id/saveanswers").post(saveAnswers);
-router.route("/:id/countanswers").get(countAnswersAverage);
-router.route("/:id/countanswersraters").get(countRatersAnswersAverage);
+router.route("/saveanswers").post(saveAnswers);
+router.route("/countanswers").get(countAnswersAverage);
+router.route("/countanswersraters").get(countRatersAnswersAverage);
 module.exports = router;
